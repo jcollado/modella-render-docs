62eb41ae9a2ab65bfbd8e2d2cffe59a09fd92f15
Javier Collado <javier.collado@gmail.com>
2015-09-18 18:23:09 +0200

    Add JSON test cases

--- test.js
+++ test.js
@@ -8,11 +8,65 @@ var modelName = 'MyModel';
 test('Model with no attributes', function(t) {
   var model = modella(modelName);
   var renderer = new ModelRenderer(model);
-  t.equal(renderer.toString(), modelName);
+  t.deepEqual(
+      renderer.toJSON(),
+      {
+        modelName: modelName,
+        attrs: {}
+      });
   t.end();
 });
 
 test('Model with attributes', function(t) {
+  var attributes = ['a', 'b', 'c'];
+  var model = modella(modelName);
+  var renderer;
+
+  attributes.forEach(function(attr) {
+    model.attr(attr);
+  });
+  renderer = new ModelRenderer(model);
+
+  t.deepEqual(
+      renderer.toJSON(),
+      {
+        modelName: modelName,
+        attrs: {
+          a: {},
+          b: {},
+          c: {}
+        }
+      });
+  t.end();
+});
+
+test('Model with ID key', function(t) {
+  var model = modella(modelName);
+  var renderer;
+
+  model.attr('id');
+  renderer = new ModelRenderer(model);
+  t.deepEqual(
+      renderer.toJSON(),
+      {
+        modelName: modelName,
+        attrs: {
+          id: {
+            primaryKey: true
+          }
+        }
+      });
+  t.end();
+});
+
+test('Render model with no attributes to string', function(t) {
+  var model = modella(modelName);
+  var renderer = new ModelRenderer(model);
+  t.equal(renderer.toString(), modelName);
+  t.end();
+});
+
+test('Render model with attributes to string', function(t) {
   var attributes = ['a', 'b', 'c'];
   var model = modella(modelName);
   var renderer;
@@ -28,7 +82,7 @@ test('Model with attributes', function(t) {
   t.end();
 });
 
-test('Model with ID key', function(t) {
+test('Render model with ID key to string', function(t) {
   var model = modella(modelName);
   var renderer;
 
